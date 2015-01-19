8b17cfa72bee630895297931a74223063fb3bfc4
heartyoh <heartyoh@gmail.com>
2015-01-19 20:05:52 +0900

    mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 465,
+    # domain:               'hatiolab.com',
+    user_name:            'heartyoh@hatiolab.com',
+    password:             ENV['HATIOLAB_MAIL_PASSWORD'],
+    # authentication:       'plain',
+    tls: true,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,4 +87,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 end
