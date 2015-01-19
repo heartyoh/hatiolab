3ed260cdc5734eb5f19ab4ceedf14d0a5e9ec402
heartyoh <heartyoh@gmail.com>
2015-01-19 11:30:34 +0900

    docker

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -6,6 +6,9 @@
 cd /hatiolab
 source /etc/profile.d/rvm.sh
 
+# Reset database : generate database, table, seed data
+bundle exec rake db:create RAILS_ENV=production
+
 export SECRET_KEY_BASE=b6762c31541007db67761bd614a61222fae847af08ce4ed9aa2fe641f2be57042fc0c00505eb09fcd711c9d8f8a0c376e2f6d42a1a3be104616272de99baf643
 
 # Start Unicorn Server as a deamon
